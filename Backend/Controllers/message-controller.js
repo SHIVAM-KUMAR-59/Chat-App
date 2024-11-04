d2cd5e85b58ba414938bdc82ea96d3dc7265a6b5
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-11-04 12:56:04 +0530

    Send Message Route Done

--- Backend/Controllers/message-controller.js
+++ Backend/Controllers/message-controller.js
@@ -1,6 +1,45 @@
+import MessageSchema from '../Schemas/MessageSchema.js'
+
 class MessageController {
-  static sendMessage = (req, res) => {
-    res.sendStatus(200)
+  static sendMessage = async (req, res) => {
+    // Destructure all the details
+    const { chatId, content, media, replyTo } = req.body
+    const sender = req.user.id.toString()
+
+    if (!chatId) {
+      return res.status(401).send({
+        status: 'failure',
+        message: 'ChatId is required',
+      })
+    }
+
+    if ((!content || content === '') && !media) {
+      return res.status(401).send({
+        status: 'failure',
+        message: 'Empty messages cannot be sent',
+      })
+    }
+
+    try {
+      const newMessage = await MessageSchema.create({
+        chatId,
+        sender,
+        content,
+        media,
+        replyTo,
+      })
+
+      res.status(201).send({
+        status: 'success',
+        message: 'Message created successfully',
+        data: newMessage,
+      })
+    } catch (error) {
+      return res.status(401).send({
+        status: 'failure',
+        message: 'Error sending message',
+      })
+    }
   }
 }
 
